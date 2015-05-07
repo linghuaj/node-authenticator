9ff4b480f0e9898053991311c98bde2e4c36aa08
Linghua Jin <ljin@walmartlabs.com>
2015-05-07 15:34:52 -0700

    authenticator started

---
+++ index.js
@@ -0,0 +1,134 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let cookieParser = require('cookie-parser')
+let session = require('express-session')
+let passport = require('passport')
+let LocalStrategy = require('passport-local').Strategy
+let bcrypt = require('bcrypt')
+let nodeify = require('bluebird-nodeify')
+let flash = require('connect-flash')
+let mongoose = require('mongoose')
+let User = require('./user')
+// let morgan = require('morgan')
+require('songbird')
+mongoose.connect('mongodb://127.0.0.1:27017/social-authenticator')
+
+// const NODE_ENV = process.env.NODE_ENV || 'dev'
+const PORT = process.env.PORT || 8000
+const SALT = bcrypt.genSaltSync(10)
+
+let userConst = {
+    email: 'foo@foo.com',
+    password: bcrypt.hashSync('asdf', SALT)
+}
+
+
+
+
+let app = express()
+app.use(flash())
+// Read cookies, required for sessions
+app.use(cookieParser('ilovethenodejs'))
+// Get POST/PUT body information (e.g., from html forms like login)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// In-memory session support, required by passport.session()
+// http://passportjs.org/guide/configure/
+app.use(session({
+  secret: 'ilovethenodejs',
+  resave: true,
+  saveUninitialized: true
+}))
+
+// Use the passport
+// middleware to enable passport
+app.use(passport.initialize())
+
+// Enable passport persistent sessions
+app.use(passport.session())
+
+
+passport.use(new LocalStrategy({
+    // Use "email" field instead of "username"
+    usernameField: 'email',
+    // We'll need this later
+    failureFlash: true
+}, (email, password, callback) => {
+	nodeify(async ()=>{
+        if (email !== userConst.email) {
+            return [false, {message: 'Invalid username'}]
+        }
+
+        if (!await bcrypt.promise.compare(password, userConst.password)) {
+            return [false, {message: 'Invalid password'}]
+        }
+        console.log(">< success")
+    // Use spread option when returning multiple values
+    }(), callback, {spread: true})
+}))
+
+
+passport.use('local-signup', new LocalStrategy({
+   // Use "email" field instead of "username"
+   usernameField: 'email'
+}, (email, password, callback) => {
+    nodeify(async ()=>{
+        email = (email || '').toLowerCase()
+        // Is the email taken?
+        if (await User.findOne({email}).exec()) {
+            return [false, {message: 'That email is already taken.'}]
+        }
+
+        // create the user
+        let user = new User()
+        // Use a password hash instead of plain-text
+        user.password = await bcrypt.promise.hash(password, SALT)
+        console.log(">< user saved")
+        return await user.save()
+
+    }(), callback, {spread: true})
+}))
+
+passport.serializeUser(function(user, callback) {
+	console.log(">< user", user)
+    // Use email since id doesn't exist
+    callback(null, user)
+})
+
+passport.deserializeUser(function(id, callback) {
+    // return the hardcoded user
+
+    callback(null, userConst)
+})
+
+
+
+// start server
+app.listen(PORT, ()=> console.log(`Listening @ http://127.0.0.1:${PORT}`))
+
+// And add the following just before app.listen
+// Use ejs for templating, with the default directory /views
+app.set('view engine', 'ejs')
+
+
+
+// And add your root route after app.listen
+app.get('/', (req, res) => res.render('index.ejs', {message: req.flash('error')}))
+// And add your root route after app.listen
+app.get('/profile', (req, res) => res.render('profile.ejs', {}))
+
+// process the login form
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/profile',
+    failureRedirect: '/',
+    failureFlash: true
+}))
+
+// process the signup form
+app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/',
+    failureFlash: true
+}))
+
