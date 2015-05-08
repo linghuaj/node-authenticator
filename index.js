8d91f42186299d0e53b48646df6d029057312bf0
Linghua Jin <ljin@walmartlabs.com>
2015-05-07 17:56:23 -0700

    show userid in profile info

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-   let userConst = {
-        email: 'foo@foo.com',
-        password: bcrypt.hashSync('asdf', SALT)
-    }
+let userConst = {
+    email: 'foo@foo.com',
+    password: bcrypt.hashSync('asdf', SALT)
+}
 
 
 // In-memory session support, required by passport.session()
@@ -55,10 +55,10 @@ passport.use('local-simple', new LocalStrategy({
     // We'll need this later
     failureFlash: true
 }, (email, password, callback) => {
-	// let userConst = {
-	// 	email: 'foo@foo.com',
-	// 	password: bcrypt.hashSync('asdf', SALT)
-	// }
+    // let userConst = {
+    // 	email: 'foo@foo.com',
+    // 	password: bcrypt.hashSync('asdf', SALT)
+    // }
 
     nodeify(async() => {
         if (email !== userConst.email) {
@@ -94,18 +94,26 @@ passport.use('local-login', new LocalStrategy({
     failureFlash: true
 }, (email, password, callback) => {
     nodeify(async() => {
-      if (!email) return [false, {message: 'Invalid email.'}]
-      email = email.toLowerCase()
-      // Lookup user by email address
-      let user = await User.findOne({email}).exec()
-      // Show error if the email does not match any users
-      if (!user) return [false, {message: 'email does not match any users'}]
-      // Show error if the hashed password does not match the stored hash
-      if (!await bcrypt.promise.compare(password, user.password)) {
-        return [false, {message: 'Invalid password.'}]
-      }
-      // Return value will be set to req.user
-      return user
+        if (!email) return [false, {
+            message: 'Invalid email.'
+        }]
+        email = email.toLowerCase()
+        // Lookup user by email address
+        let user = await User.findOne({
+                email
+            }).exec()
+            // Show error if the email does not match any users
+        if (!user) return [false, {
+                message: 'email does not match any users'
+            }]
+            // Show error if the hashed password does not match the stored hash
+        if (!await bcrypt.promise.compare(password, user.password)) {
+            return [false, {
+                message: 'Invalid password.'
+            }]
+        }
+        // Return value will be set to req.user
+        return user
     }(), callback, {
         spread: true
     })
@@ -142,23 +150,30 @@ passport.use('local-signup', new LocalStrategy({
 
 //why serialize
 passport.serializeUser(function(user, callback) {
-	console.log(">< serialize user")
+    console.log(">< serialize user")
     // Use email since id doesn't exist
     callback(null, user._id)
 })
 
 //why deserialize
 passport.deserializeUser(function(id, callback) {
-	console.log(">< deserialize id", id)
-	nodeify(async() => {
-		let user = await User.findById(id).exec()
-		//user = await User.promise.findById(id)
-		console.log(">< user deserialize", user)
-		return user
-	}(), callback, {spread: true})
+    console.log(">< deserialize id", id)
+    nodeify(async() => {
+        let user = await User.findById(id).exec()
+            //user = await User.promise.findById(id)
+        console.log(">< user deserialize", user)
+        return user
+    }(), callback, {
+        spread: true
+    })
 
 })
 
+function isLoggedIn(req, res, next) {
+    //passport handled?
+    if (req.isAuthenticated()) return next()
+    res.redirect('/')
+}
 
 
 // start server
@@ -175,7 +190,14 @@ app.get('/', (req, res) => res.render('index.ejs', {
     message: req.flash('error')
 }))
 // And add your root route after app.listen
-app.get('/profile', (req, res) => res.render('profile.ejs', {}))
+app.get('/profile', isLoggedIn, (req, res) => {
+    // console.log("><req", req)
+    // console.log("><res", res)
+    return res.render('profile.ejs', {
+        email: req.user.email,
+        id: req.user._id
+    })
+})
 
 // process the login form
 app.post('/login', passport.authenticate('local-login', {
